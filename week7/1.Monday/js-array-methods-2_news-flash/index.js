87502646d1c17c78009a3456063f652ec5eb74ec
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-24 11:23:12 +0100

    mono repo implementation

---
+++ week7/1.Monday/js-array-methods-2_news-flash/index.js
@@ -0,0 +1,25 @@
+import { news } from "./utils/news.js";
+import { Card } from "./components/Card/Card.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+);
+
+// Part 2 - start here
+const sortedNews = filteredNews;
+
+sortedNews.sort((card1, card2) => card1.body.length - card2.body.length);
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);

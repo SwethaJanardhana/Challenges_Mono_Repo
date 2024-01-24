45fd45e97c9074860f6a883329a743c3405eb652
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-24 17:04:45 +0100

    fix

--- week7/3.Wednesday/js-error-handling_fetch/js/index.js
+++ week7/3.Wednesday/js-error-handling_fetch/js/index.js
@@ -6,24 +6,34 @@ const errorMessageElement = document.querySelector(".error");
 async function getUser(url) {
   const response = await fetch(url);
   if (!response.ok) {
+    console.log("Network error");
+    errorMessageElement.textContent = "Network error";
+    userElement.textContent = "";
     return null;
   }
-  const json = await response.json();
-  return json.data;
+  try {
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.error(error);
+    errorMessageElement.textContent = "Ooops something went wrong.";
+    userElement.textContent = "";
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
-    try {
-      errorMessageElement.innerHTML = "";
-      const user = await getUser(event.target.dataset.url);
-      userElement.innerHTML = `
-      <h2>${user.first_name} ${user.last_name}</h2>
-      <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
-      `;
-    } catch (error) {
-      userElement.innerHTML = "";
-      errorMessageElement.innerHTML = `Cannot fetch this user details : ${error}`;
+    const user = await getUser(event.target.dataset.url);
+
+    if (!user) {
+      console.error("User not found");
+      return;
     }
+
+    userElement.innerHTML = `
+    <h2>${user.first_name} ${user.last_name}</h2>
+    <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+    `;
   })
 );

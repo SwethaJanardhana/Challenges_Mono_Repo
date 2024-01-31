68734223364862ef961ccda866dfe88e940227a7
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-31 11:55:33 +0100

    Challenge completed

---
+++ week8/react-basics_attributes/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <div>
+      <article className="article">
+        <h2 className="article__title">Article with Attributes</h2>
+        <label htmlFor="name">Name :</label>
+        <input id="name" />
+        <a
+          href="https://www.neuefische.de/bootcamp/java-development?utm_source=sea_google&utm_medium=search&utm_campaign=nonbrand_java_bundesweit&gad_source=1&gclid=CjwKCAiA_OetBhAtEiwAPTeQZ4hadz-h7lV0p2a98OO1U3kdnDJYIHBzXhQevr5H4GYl0pIZkSoBdBoCLXkQAvD_BwE"
+          className="article__link"
+        >
+          Click Here!
+        </a>
+      </article>
+    </div>
+  );
+}

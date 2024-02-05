816a47fab0ab9971400e2802babdcefb9f045965
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-05 11:40:42 +0100

    Solved

---
+++ week9/1.Moonday/react-nesting_navigation/src/components/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../img/logo.jpg";
+import Image from "./Image";
+import Link from "./Link";
+
+export default function Logo() {
+  return (
+    <Link href="#">
+      <Image src={logo} alt="logo" />
+    </Link>
+  );
+}

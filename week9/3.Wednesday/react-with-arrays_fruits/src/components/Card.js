5fc41ff2b15256668f28eb48bcda2c89e10b2783
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-07 12:06:51 +0100

    inline styling removed

--- week9/3.Wednesday/react-with-arrays_fruits/src/components/Card.js
+++ week9/3.Wednesday/react-with-arrays_fruits/src/components/Card.js
@@ -1,9 +1,5 @@
 import "./Card.css";
 
 export default function Card({ name, color }) {
-  return (
-    <p className="card" style={{ backgroundColor: color }}>
-      {name}
-    </p>
-  );
+  return <p className={`card card--${color}`}>{name}</p>;
 }

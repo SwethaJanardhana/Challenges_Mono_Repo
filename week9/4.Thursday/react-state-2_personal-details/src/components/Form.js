519d62b0bd1e6d825bb3632d47c5c8f645e29b40
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-08 11:11:52 +0100

    Solved

---
+++ week9/4.Thursday/react-state-2_personal-details/src/components/Form.js
@@ -0,0 +1,27 @@
+export default function Form({ setName, setEmail }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const formData = Object.fromEntries(form);
+    setName(formData.name);
+    setEmail(formData.email);
+    event.target.reset();
+  }
+
+  return (
+    <form
+      className="form"
+      aria-labelledby="user-details"
+      onSubmit={handleSubmit}
+    >
+      <h2 id="user-details">Please enter your details here!</h2>
+      <label htmlFor="name">Name: </label>
+      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <label htmlFor="email">Email: </label>
+      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <button className="form__submit-button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}

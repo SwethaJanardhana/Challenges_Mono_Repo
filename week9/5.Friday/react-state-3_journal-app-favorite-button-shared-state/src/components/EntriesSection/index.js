38970a35b64053bccf7bf02f863a1df3ac736698
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-09 16:39:13 +0100

    Solved

---
+++ week9/5.Friday/react-state-3_journal-app-favorite-button-shared-state/src/components/EntriesSection/index.js
@@ -0,0 +1,38 @@
+import "./EntriesSection.css";
+import Divider from "../Divider";
+import Entry from "../Entry";
+import Tabs from "../Tabs";
+import Tab from "../Tab";
+import Badge from "../Badge";
+import { Fragment } from "react";
+
+export default function EntriesSection({ entries, onToggleFavorite }) {
+  const favouritesCount = entries.filter((entry) => entry.isFavorite === true);
+  return (
+    <section className="entries-section">
+      <Tabs>
+        <Tab active>
+          All Entries <Badge isActive>{entries.length}</Badge>
+        </Tab>
+        <Tab>
+          Favorites <Badge>{favouritesCount.length}</Badge>
+        </Tab>
+      </Tabs>
+      <div className="entries-section__entries">
+        {entries.map((entry, index) => (
+          <Fragment key={entry.id}>
+            {index > 0 ? <Divider /> : null}
+            <Entry
+              date={entry.date}
+              motto={entry.motto}
+              notes={entry.notes}
+              onToggleFavorite={onToggleFavorite}
+              isFavorite={entry.isFavorite}
+              id={entry.id}
+            />
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}

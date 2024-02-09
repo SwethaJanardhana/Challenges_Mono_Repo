89c43a82dc5949646cffe3dbc9909981d9704e12
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-09 17:56:03 +0100

    Solved

---
+++ week9/5.Friday/react-state-3_journal-app-filter/src/components/EntriesSection/index.js
@@ -0,0 +1,45 @@
+import "./EntriesSection.css";
+import Divider from "../Divider";
+import Entry from "../Entry";
+import Tabs from "../Tabs";
+import Tab from "../Tab";
+import Badge from "../Badge";
+import { Fragment } from "react";
+
+export default function EntriesSection({
+  entries,
+  onToggleFavorite,
+  onShowAllEntries,
+  onShowFavoriteEntries,
+  filter,
+}) {
+  return (
+    <section className="entries-section">
+      <Tabs>
+        <Tab isActive={filter === "all"} onClick={onShowAllEntries}>
+          All Entries{" "}
+          {filter === "all" && <Badge isActive>{entries.length}</Badge>}
+        </Tab>
+        <Tab isActive={filter === "favorites"} onClick={onShowFavoriteEntries}>
+          Favorites{" "}
+          {filter === "favorites" && <Badge isActive>{entries.length}</Badge>}
+        </Tab>
+      </Tabs>
+      <div className="entries-section__entries">
+        {entries.map((entry, index) => (
+          <Fragment key={entry.id}>
+            {index > 0 ? <Divider /> : null}
+            <Entry
+              date={entry.date}
+              motto={entry.motto}
+              notes={entry.notes}
+              onToggleFavorite={onToggleFavorite}
+              id={entry.id}
+              isFavorite={entry.isFavorite}
+            />
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}

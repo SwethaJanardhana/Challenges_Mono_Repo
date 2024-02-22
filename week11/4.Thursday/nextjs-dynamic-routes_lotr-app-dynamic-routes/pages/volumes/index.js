8d0fce60c535424dce21305dce153a0512cdd74c
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-22 11:18:59 +0100

    Solved

---
+++ week11/4.Thursday/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Head from "next/head";
+import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+
+export default function Volumes() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomVolumePage() {
+    const randomVolumesIndex = getRandomElement(volumes);
+    const slug = randomVolumesIndex.slug;
+    router.push(`volumes/${slug}`);
+  }
+  return (
+    <>
+      <Head>
+        <title>The Lord of the Rings</title>
+      </Head>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleRandomVolumePage}>Random Volume</button>
+    </>
+  );
+}

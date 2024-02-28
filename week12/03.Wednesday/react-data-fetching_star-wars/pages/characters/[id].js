62f89fd88b3d9b252289f7257965bd9b96d52c93
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-28 12:05:50 +0100

    Solved

---
+++ week12/03.Wednesday/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,41 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  color: white;
+`;
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+  const URL = `https://swapi.dev/api/people/${id}`;
+
+  const fetcher = (URL) => fetch(URL).then((response) => response.json());
+
+  const { data: character, isLoading, error } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  if (error) {
+    return <div>Error : {error.message}</div>;
+  }
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={character.name}
+        height={character.height}
+        eyeColor={character.eye_color}
+        birthYear={character.birth_year}
+      />
+      <br></br>
+      <StyledLink href="/">Back</StyledLink>
+    </Layout>
+  );
+}

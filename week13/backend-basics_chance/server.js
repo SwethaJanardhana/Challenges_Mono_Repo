f72a12f7cca3de5515601e3cbd0882ae4ecf7207
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-03-11 10:38:35 +0100

    monorepo update

--- week13/backend-basics_chance/server.js
+++ week13/backend-basics_chance/server.js
@@ -2,11 +2,12 @@ import { createServer } from "node:http";
 import Chance from "chance";
 const chance = new Chance();
 
-const name = chance.name();
-const age = chance.age();
-const profession = chance.profession();
-
 function handler(request, response) {
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+  console.log(chance);
+
   response.statusCode = 200;
   response.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
